7c117b6f46ddefa53a2519f3a18e3c3c4324998c
Alberto Cruzado Pleite <alberto.cruzadoo@unit4.com>
2023-01-09 17:14:29 +0100

    completing the challenge

--- 2_Automate_Report_Generation_Using_Google_APIs/2_Using_Google_Chart_API/readInData.py
+++ 2_Automate_Report_Generation_Using_Google_APIs/2_Using_Google_Chart_API/readInData.py
@@ -1,5 +1,9 @@
 import csv
 
+#final desired format
+# - Charts [["Test Name",<diff from avg>]]
+# - spreadsheet [["Test Name",<current run time>]]
+
 timing_data = []
 with open ('TestTimingData.csv') as csv_file:
     file_reader = csv.reader(csv_file)
