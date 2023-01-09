30babbf8462039178054b426683a878aa6e9603a
Alberto Cruzado Pleite <alberto.cruzadoo@unit4.com>
2023-01-09 16:37:21 +0100

    completing the 2nd excercise of this part of the course

--- 2_Automate_Report_Generation_Using_Google_APIs/1_Gathering_data/readInData.py
+++ 2_Automate_Report_Generation_Using_Google_APIs/1_Gathering_data/readInData.py
@@ -18,8 +18,13 @@ table_data = [["Test Name", "Run Time (s)"]]
 #since the 1st row of the CSV is for the name of the columns, we start at index 1 so that se would skip the 1st row. 
 for row in timing_data[1:]:
     test_name = row[0]
-    current_run_time = row[1]
-    avg_run_time = row[2]
+    if not row[1] or not row[2]:
+        continue
+    current_run_time = float(row[1])
+    avg_run_time = float(row[2])
     diff_from_avg = avg_run_time - current_run_time
     column_chart_data.append([test_name,diff_from_avg])
     table_data.append([test_name,current_run_time])
+
+print(column_chart_data)
+print(table_data)
