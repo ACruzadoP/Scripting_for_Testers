bc801d6eed43f1e01d6b37e50b7ce3f6d3b7aab6
Alberto Cruzado Pleite <alberto.cruzadoo@unit4.com>
2023-02-13 16:12:33 +0100

    completing the last excercise for the 3rd module of the course

--- 3_Automate_Test_with_Selenium/4_Initial_page_setup/SeleniumPageSetup.py
+++ 3_Automate_Test_with_Selenium/4_Initial_page_setup/SeleniumPageSetup.py
@@ -10,4 +10,12 @@ driver = webdriver.Chrome(options=options)
 driver.get('http://automationpractice.pl/index.php?id_category=3&controller=category')
 driver.maximize_window()
 
+product_containers = driver.find_elements("class name","product-container")
 
+for index,product_container in enumerate(product_containers):
+    hover = ActionChains(driver).move_to_element(product_container).perform()
+    driver.find_element("xpath", '//*[@id="center_column"]/ul/li[%s]/div/div[2]/div[2]/a[1]/span'%(index+1)).click()
+    time.sleep(1)
+    driver.find_element("xpath", '//*[@id="layer_cart"]/div[1]/div[2]/div[4]/span/span').click()
+    time.sleep(1)
+    
